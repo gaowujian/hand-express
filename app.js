7121461106e8e1839b837c1cab407b513c4e65b2
tony <gwjlshebei@gmail.com>
2020-12-23 22:09:08 +0800

    分割应用

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require("./express");
 const app = express();
 
-// 在没有express之前，我们都是通过对req.url的pathname进行不同资源的处理并返回
-// 这个概念就是路由
-// 在请求和相应中间，可以做一些事，这叫做中间件
-
 app.get("/", function (req, res) {
-  res.end("///");
+  res.end("home ");
 });
 app.get("/login", function (req, res) {
-  res.end("loging");
+  res.end("login");
 });
-
-// all表示，不管是什么方法，都可以匹配到这个路径
-// 星号* 表示不管是什么路径都可以匹配
-// 可以用来兜底执行
-
-// app.all("*", (req, res) => {
-//   res.end(req.url + "404");
-// });
 
 app.listen(3000, () => {
-  console.log("start on 3000!");
+  console.log("start on http://localhost:3000 !");
 });
-1;
--- express.js
+++ express.js
@@ -1,40 +1,7 @@
-const http = require("http");
-const url = require("url");
-const routes = [
-  {
-    path: "*",
-    method: "all",
-    handler: function (req, res) {
-      res.end(`${req.url}is not found`);
-    },
-  },
-];
+const Application = require("./Application");
+
 function createApplication() {
-  return {
-    get: function (path, handler) {
-      routes.push({
-        path,
-        method: "get",
-        handler,
-      });
-    },
-    listen: function (...args) {
-      const server = http.createServer((req, res) => {
-        const { pathname } = new URL(req.url, `http://${req.headers.host}`);
-        const requestMethod = req.method.toLocaleLowerCase();
-        for (let i = 0; i < routes.length; i++) {
-          const { path, method, handler } = routes[i];
-          //  路由就是路径和方法都匹配
-          if (path === pathname && method === requestMethod) {
-            return handler(req, res);
-          }
-        }
-        // 如果的都匹配不到，就走默认路由
-        routes[0].handler(req, res);
-      });
-      server.listen(...args);
-    },
-  };
+  return new Application();
 }
 
 module.exports = createApplication;
